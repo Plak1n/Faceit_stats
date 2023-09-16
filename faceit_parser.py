ce49909ea7cb3a6d83107d01618d9d0669ff2599
Plak1n <ms.joker590@gmail.com>
2023-09-16 17:37:18 +0300

    testing

--- faceit_parser.py
+++ faceit_parser.py
@@ -23,5 +23,20 @@ def get_nicknames(url: str, team_number: int):
     required_nicknames = [all_nicknames[0],all_nicknames[2], all_nicknames[4], all_nicknames[6], all_nicknames[8]]
     return required_nicknames
 
-members = get_nicknames("https://www.faceit.com/en/csgo/room/1-0ec43dc8-be2f-4e97-9d1b-5e82d7db0f77",1)
-print(members)
+# url = input("Enter url of you faceit match: ")
+# team_number = int(input("Enter team number. Team on the left has number 1, team on the right has number 2: "))
+# nicknames = get_nicknames(url,team_number)
+# profile_links = []
+# for nk in nicknames:
+#     profile_links.append(f"https://www.faceit.com/en/players/{nk}")
+# print(profile_links)
+
+if __name__ == "__main__":
+    url = "https://www.faceit.com/en/csgo/room/1-0ec43dc8-be2f-4e97-9d1b-5e82d7db0f77"
+    team_number = 1
+    nicknames = get_nicknames(url,team_number)
+    profile_links = []
+    for nk in nicknames:
+         profile_links.append(f"https://www.faceit.com/en/players/{nk}")
+    print(profile_links)
+         
